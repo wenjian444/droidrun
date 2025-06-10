8e9aa8f01de036e9881744bcf63111ddcb2962f9
tbeck <beckmann.timo@icloud.com>
2025-06-10 18:08:36 +0200

    chore: disable default logger config. users can decide for themselfes

--- droidrun/agent/__init__.py
+++ droidrun/agent/__init__.py
@@ -1,6 +1,6 @@
-import logging
+#import logging
 
-logger = logging.getLogger("droidrun")
-logger.propagate = False  # Don't send to root logger
-logger.handlers = []      # No handlers by default
-logger.setLevel(logging.INFO)  # Or WARNING
+#logger = logging.getLogger("droidrun")
+#logger.propagate = False  # Don't send to root logger
+#logger.handlers = []      # No handlers by default
+#logger.setLevel(logging.INFO)  # Or WARNING
