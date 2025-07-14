56659fbbd7e88398aecfc22491e60dc586039834
wenjian444 <44427087@qq.com>
2025-07-15 01:40:05 +0800

    Revert "Add files via upload"

--- .github/workflows/droidrun-0.3.2/droidrun/agent/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Utility modules for DroidRun agents.
-"""
--- .github/workflows/droidrun-0.3.2/droidrun/telemetry/__init__.py
+++
@@ -1,4 +0,0 @@
-from .tracker import capture, flush, print_telemetry_message
-from .events import DroidAgentInitEvent, DroidAgentFinalizeEvent
-
-__all__ = ["capture", "flush", "DroidAgentInitEvent", "DroidAgentFinalizeEvent", "print_telemetry_message"]
